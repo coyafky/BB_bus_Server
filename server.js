c7843404ccab176cc81e73dc5e458182050edca8
coya <2233606771@qq.com>
2024-07-02 14:28:36 +0800

    change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import { MongoClient } from 'mongodb';
-import cors from 'cors';
+import express from "express";
+import { MongoClient } from "mongodb";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,36 +8,40 @@ const port = process.env.PORT || 3000;
 // 启用 CORS 中间件
 app.use(cors());
 
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
-  
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+app.get("/favicon.ico", (req, res) => {
+  res.status(204).end();
+});
 
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let db;
 
 async function connectToDatabase() {
   try {
     await client.connect();
-    db = client.db('BB_bus'); // 替换为你的数据库名称
-    console.log('Connected to MongoDB');
+    db = client.db("BB_bus"); // 替换为你的数据库名称
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   }
 }
 
 // 在应用启动时连接数据库
 connectToDatabase().catch(console.error);
 
-app.get('/cities', async (req, res) => {
+app.get("/cities", async (req, res) => {
   try {
     if (!db) {
       await connectToDatabase();
     }
-    const collection = db.collection('cities'); // 替换为你的集合名称
+    const collection = db.collection("cities"); // 替换为你的集合名称
 
     const query = {}; // 你可以根据需要添加查询条件
     const options = {}; // 你可以根据需要添加查询选项
@@ -45,20 +49,20 @@ app.get('/cities', async (req, res) => {
     const cursor = collection.find(query, options);
     const result = await cursor.toArray();
 
-    console.log('Fetched cities:', result);
+    console.log("Fetched cities:", result);
     res.json(result);
   } catch (error) {
-    console.error('Error fetching cities:', error);
-    res.status(500).send('Server Error');
+    console.error("Error fetching cities:", error);
+    res.status(500).send("Server Error");
   }
 });
 
-app.get('/routes', async (req, res) => {
+app.get("/routes", async (req, res) => {
   try {
     if (!db) {
       await connectToDatabase();
     }
-    const collection = db.collection('routes'); // 替换为你的集合名称
+    const collection = db.collection("routes"); // 替换为你的集合名称
 
     let query = {};
 
@@ -67,11 +71,11 @@ app.get('/routes', async (req, res) => {
     const cursor = collection.find(query, options);
     const result = await cursor.toArray();
 
-    console.log('Fetched routes:', result);
+    console.log("Fetched routes:", result);
     res.json(result);
   } catch (error) {
-    console.error('Error fetching routes:', error);
-    res.status(500).send('Server Error');
+    console.error("Error fetching routes:", error);
+    res.status(500).send("Server Error");
   }
 });
 
